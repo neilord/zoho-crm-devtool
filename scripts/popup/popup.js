c9c6b36d26bfae326bc88c6760f8ebc6f81112bf
neilord <arshostak@gmail.com>
2023-05-26 20:27:34 +0200

    Complete Extension Popup UI

--- scripts/popup/popup.js
+++ scripts/popup/popup.js
@@ -11,7 +11,7 @@ async function toggleAnimations(enabled) {
 }
 
 async function toggleExtensionActivation() {
-  const mainSwitch = document.querySelector('#extension-activation-switch .toggle');
+  const mainSwitch = document.querySelector('#extension-activation-switch .toggle-switch');
   await chrome.storage.sync.get(['extensionActivated']).then((result) => {
     const extensionActivated = result.extensionActivated;
     if (extensionActivated == true) {
@@ -20,7 +20,7 @@ async function toggleExtensionActivation() {
       mainSwitch.classList.remove('toggle-on');
     } else {
       chrome.storage.sync.set({
-        extensionActivated: document.querySelector('.toggle.toggle-on'),
+        extensionActivated: document.querySelector('.toggle-switch.toggle-on'),
       });
     }
   });
@@ -49,15 +49,18 @@ async function setInitialSettings() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+  createRemoveScriptElement('variables.css');
   setInitialSettings();
 
   // Main switch toggler
-  const mainSwitch = document.querySelector('#extension-activation-switch .toggle');
-  mainSwitch.addEventListener('click', () => {
-    mainSwitch.classList.toggle('toggle-on');
-    chrome.storage.sync.get(['extensionActivated']).then((result) => {
-      chrome.storage.sync.set({
-        extensionActivated: !result.extensionActivated,
+  const mainSwitches = document.querySelectorAll('.toggle-switch');
+  mainSwitches.forEach(function (mainSwitch) {
+    mainSwitch.addEventListener('click', () => {
+      mainSwitch.classList.toggle('toggle-on');
+      chrome.storage.sync.get(['extensionActivated']).then((result) => {
+        chrome.storage.sync.set({
+          extensionActivated: !result.extensionActivated,
+        });
       });
     });
   });
@@ -72,3 +75,36 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+async function createRemoveScriptElement(file, create = true) {
+  const name = file.split('.')[0];
+  const type = file.split('.')[1];
+
+  const oldElement = document.getElementById(name);
+  if (oldElement) {
+    oldElement.remove();
+  }
+
+  if (create) {
+    const url = chrome.runtime.getURL('scripts/' + file);
+    const newElement = document.createElement(type == 'js' ? 'script' : 'link');
+    newElement.id = name;
+    newElement.setAttribute(type == 'js' ? 'src' : 'href', url);
+    newElement.setAttribute(
+      type == 'js' ? 'type' : 'rel',
+      type == 'js' ? 'text/javascript' : 'stylesheet'
+    );
+    document.body.appendChild(newElement);
+  }
+}
