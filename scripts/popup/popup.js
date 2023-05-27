8cfd70db323bfd568fba3d1b99848d9a8bb6e862
neilord <arshostak@gmail.com>
2023-05-27 14:37:03 +0200

    Add Settings Save

--- scripts/popup/popup.js
+++ scripts/popup/popup.js
@@ -1,29 +1,9 @@
-async function toggleAnimations(enabled) {
-  const elements = Array.from(document.body.getElementsByTagName('*'));
-  elements.forEach(function (element) {
-    if (enabled) {
-      element.style.transition = element.dataset.originalTransition;
-    } else {
-      element.dataset.originalTransition = element.style.transition;
-      element.style.transition = 'none';
-    }
-  });
-}
-
-async function toggleExtensionActivation() {
-  const mainSwitch = document.querySelector('#extension-activation-switch .toggle-switch');
-  await chrome.storage.sync.get(['extensionActivated']).then((result) => {
-    const extensionActivated = result.extensionActivated;
-    if (extensionActivated == true) {
-      mainSwitch.classList.add('toggle-on');
-    } else if (extensionActivated == false) {
-      mainSwitch.classList.remove('toggle-on');
-    } else {
-      chrome.storage.sync.set({
-        extensionActivated: document.querySelector('.toggle-switch.toggle-on'),
-      });
-    }
-  });
+async function loadCSSVariables() {
+  const file = document.createElement("link");
+  file.setAttribute("rel", "stylesheet");
+  file.setAttribute("type", "text/css");
+  file.setAttribute("href", chrome.runtime.getURL('scripts/variables.css'));
+  document.head.appendChild(file);
 }
 
 async function setTabsPossitions() {
@@ -41,29 +21,86 @@ async function setTabsPossitions() {
   });
 }
 
-async function setInitialSettings() {
-  await toggleAnimations(false);
-  await toggleExtensionActivation();
-  await setTabsPossitions();
-  await toggleAnimations(true);
+async function toggleAnimations(enabled) {
+  const elements = Array.from(document.body.getElementsByTagName('*'));
+  elements.forEach(function (element) {
+    if (enabled) {
+      element.style.transition = element.dataset.originalTransition;
+    } else {
+      element.dataset.originalTransition = element.style.transition;
+      element.style.transition = 'none';
+    }
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  createRemoveScriptElement('variables.css');
-  setInitialSettings();
+async function setSavedSettings() {
+  await toggleAnimations(false);
 
-  // Main switch toggler
-  const mainSwitches = document.querySelectorAll('.toggle-switch');
-  mainSwitches.forEach(function (mainSwitch) {
-    mainSwitch.addEventListener('click', () => {
-      mainSwitch.classList.toggle('toggle-on');
-      chrome.storage.sync.get(['extensionActivated']).then((result) => {
-        chrome.storage.sync.set({
-          extensionActivated: !result.extensionActivated,
-        });
+  const setSetting = (setting, value) => {
+    if (setting.id.endsWith('-select')) {
+      setting.value = value;
+    } else if (setting.id.endsWith('-switch')) {
+      setting.children[0].classList[value ? 'add' : 'remove']('toggle-on');
+    }
+  };
+
+  const checkSetting = (setting, value) => {
+    if (setting.id.endsWith('-select')) {
+      return setting.value == value;
+    } else if (setting.id.endsWith('-switch')) {
+      return setting.children[0].classList.contains('toggle-on') == value;
+    }
+    return true;
+  };
+
+  const waitForSettingsSet = (checkSettings, delay) => new Promise(resolve =>
+    setTimeout(() => checkSettings() ?
+      resolve() : waitForSettingsSet(checkSettings, delay).then(resolve), delay)
+  );
+
+  await new Promise((resolve) => {
+    chrome.storage.sync.get(null, function (variables) {
+      Object.entries(variables).forEach(([key, value]) => {
+        const setting = document.getElementById(key);
+        setSetting(setting, value);
+      });
+
+      const checkSettings = () =>
+        Object.entries(variables).every(([key, value]) =>
+          checkSetting(document.getElementById(key), value));
+
+      resolve(waitForSettingsSet(checkSettings, 50));
+    });
+  });
+
+  toggleAnimations(true);
+}
+
+async function startSettingsSave() {
+  // Dropdowns
+  const settingDropdowns = document.querySelectorAll('.setting-dropdown');
+  settingDropdowns.forEach(function (settingDropdown) {
+    settingDropdown.addEventListener('change', () => {
+      chrome.storage.sync.set({ [settingDropdown.id]: settingDropdown.value });
+    });
+  });
+
+  // Switches
+  const settingSwitches = document.querySelectorAll('.setting-switch, #extension-activation-switch');
+  settingSwitches.forEach(function (settingSwitch) {
+    settingSwitch.addEventListener('click', () => {
+      chrome.storage.sync.set({
+        [settingSwitch.id]: !!document.querySelector('#' + settingSwitch.id + ':has(.toggle-on)')
       });
     });
   });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  loadCSSVariables();
+  setTabsPossitions();
+  setSavedSettings();
+  startSettingsSave();
 
   // Tabs selector
   const tabs = document.querySelectorAll('.tab');
@@ -74,37 +111,12 @@ document.addEventListener('DOMContentLoaded', function () {
       setTabsPossitions();
     });
   });
+
+  // Switch toggler
+  const settingSwitches = document.querySelectorAll('.toggle-switch');
+  settingSwitches.forEach(function (settingSwitch) {
+    settingSwitch.addEventListener('click', () => {
+      settingSwitch.classList.toggle('toggle-on');
+    });
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-async function createRemoveScriptElement(file, create = true) {
-  const name = file.split('.')[0];
-  const type = file.split('.')[1];
-
-  const oldElement = document.getElementById(name);
-  if (oldElement) {
-    oldElement.remove();
-  }
-
-  if (create) {
-    const url = chrome.runtime.getURL('scripts/' + file);
-    const newElement = document.createElement(type == 'js' ? 'script' : 'link');
-    newElement.id = name;
-    newElement.setAttribute(type == 'js' ? 'src' : 'href', url);
-    newElement.setAttribute(
-      type == 'js' ? 'type' : 'rel',
-      type == 'js' ? 'text/javascript' : 'stylesheet'
-    );
-    document.body.appendChild(newElement);
-  }
-}
