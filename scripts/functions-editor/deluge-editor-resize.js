d918f6875731a479a683fb494557a13fca897e1c
neilord <arshostak@gmail.com>
2023-06-12 17:28:55 +0200

    Add Extension Popup Switch Border Color Setting

--- scripts/functions-editor/deluge-editor-resize.js
+++ scripts/functions-editor/deluge-editor-resize.js
@@ -2,7 +2,6 @@
   const interval = setInterval(() => {
     try {
       delugeEditor.resize();
-      console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
       clearInterval(interval);
     } catch (error) {
       // delugeEditor is not available yet
